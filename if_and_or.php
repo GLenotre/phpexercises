23967e17bf35bc4d0a851a7cbab941f24d6a4f9e
Gaston Lenotre <gastonlenotre@gmail.com>
2016-02-29 11:09:09 -0600

    new php exercises

---
+++ if_and_or.php
@@ -0,0 +1,25 @@
+<?php
+
+$x = 0;
+$y = 5;
+$z = 10;
+
+
+// If $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+if ($x < $y && $y < $z) {
+	echo "{$x} < {$y} < {$z}\n";
+}
+
+if (0 < $x || $x < 10) {
+	echo "0 is less than {$x} OR {$x} is less than 10 \n";
+}
+
+// TODO:
+// repeat the if statement for $y and $z.
+
+// TODO:
+// If 0 is less than $x AND $x is less than 10
+// then echo the result as a sentence "0 is less than {$x} AND {$x} is less than 10".
+
+// TODO:
+// repeat the if statement for $y and $z.
