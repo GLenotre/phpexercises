a0bdb09b78d771be52edce1c139ed29925820f19
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-01 15:38:22 -0600

    added functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,17 @@
 <?php
 
+$a = 40;
+$b = 20;
+
+function validateAB($a, $b) {
+    if (is_numeric($a) && is_numeric($b)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 function add($a, $b)
 {
     return $a + $b;
@@ -7,17 +19,26 @@ function add($a, $b)
 
 function subtract($a, $b)
 {
-    // Add code here
+    if (validateAB($a, $b)) {
+        return $a - $b;
+    } 
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+	if (validateAB($a, $b)) {
+		return $a * $b;
+	} 
 }
 
 function divide($a, $b)
 {
-    // Add code here
+	return $a / $b;
 }
 
 // Add code to test your functions here
+echo add($a, $b) . PHP_EOL;
+echo subtract($a, $b) . PHP_EOL;
+echo multiply($a, $b) . PHP_EOL;
+echo divide($a, $b) . PHP_EOL;
+// echo modulus($a, $b) . PHP_EOL;
