1b98dae052354716c30f8afe2caf35311b5b46b5
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-01 12:56:41 -0600

    included a foreach in a foreach

--- foreach.php
+++ foreach.php
@@ -3,16 +3,26 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-	if (is_integer($thing)) {
-		echo "{$thing} is an integer\n";
-	} else if (is_float($thing)) {
-		echo "{$thing} is a float\n";
-	} else if (is_bool($thing)) {
-		echo "{$thing} is a boolean\n";
-	} else if (is_null($thing)) {
-		echo "{$thing} is null\n";
-	} else if (is_string($thing)) {
-		echo "{$thing} is a string\n";
-	}
+	// if (is_integer($thing)) {
+	// 	echo "{$thing} is an integer\n";
+	// } else if (is_float($thing)) {
+	// 	echo "{$thing} is a float\n";
+	// } else if (is_bool($thing)) {
+	// 	echo "{$thing} is a boolean\n";
+	// } else if (is_null($thing)) {
+	// 	echo "{$thing} is null\n";
+	// } else if (is_string($thing)) {
+	// 	echo "{$thing} is a string\n";
+	// }
+
+    if (is_scalar($thing)) {
+        echo ($thing) . PHP_EOL;
+	 } 
+
+	if (is_array($thing)) {
+	 	foreach ($thing as $element) {
+	 	echo ($element) . PHP_EOL;
+	 	}
+	 }
 }
 
