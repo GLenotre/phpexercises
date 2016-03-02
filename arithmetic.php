996f4f4ee085063da397c0892e1c40df85e5cd25
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-02 09:33:13 -0600

    added modulo function

--- arithmetic.php
+++ arithmetic.php
@@ -1,13 +1,13 @@
 <?php
 
-$a = 40;
-$b = 20;
+$x = 40;
+$y = 20;
 
 function validateAB($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
         return true;
     } else {
-        return false;
+        return "Your inputs must be numeric" . PHP.EOL;
     }
 }
 
@@ -33,12 +33,21 @@ function multiply($a, $b)
 
 function divide($a, $b)
 {
+    if (validateAB($a, $b)) {
 	return $a / $b;
+    }
+}
+
+function modulo($a, $b)
+{
+    if (validateAB($a, $b)) {
+    return $a % $b;
+    }
 }
 
 // Add code to test your functions here
-echo add($a, $b) . PHP_EOL;
-echo subtract($a, $b) . PHP_EOL;
-echo multiply($a, $b) . PHP_EOL;
-echo divide($a, $b) . PHP_EOL;
-// echo modulus($a, $b) . PHP_EOL;
+echo add($x, $y) . PHP_EOL;
+echo subtract($x, $y) . PHP_EOL;
+echo multiply($x, $y) . PHP_EOL;
+echo divide($x, $y) . PHP_EOL;
+echo modulo($x, $y) . PHP_EOL;
