d4f83eeeb66396668b889463735e24e3c960e5e0
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-03 15:08:29 -0600

    made alphabetical sorting optional

--- list_with_commas.php
+++ list_with_commas.php
@@ -5,16 +5,14 @@ $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mall
 $physicistsArray = explode(', ', $physicistsString);
 
 
-$physicists_array = humanizedList($physicistsArray);
+$physicists_array = humanizedList($physicistsArray, true);
 	$tony = array_pop($physicists_array);
     $famousFakePhysicists = implode(', ', $physicists_array);
 	$famousFakePhysicists = $famousFakePhysicists . ", and " . $tony;
     echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
     function humanizedList ($array, $sort = false) {
-        asort($array);
-        return $array;
-    }
-
-// print_r($physicists_array);
-
- 
+        if ($sort === true) {
+	        sort($array);
+	        return $array;	
+        }
+    } 
