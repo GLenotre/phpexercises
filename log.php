f71b3e5e1696288df734b3a1e4503f2eeb68b1d4
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-23 12:01:45 -0500

    refactored the class Log with visibility of filename and handle  to private using setters

--- log.php
+++ log.php
@@ -2,22 +2,27 @@
 class Log
 
 { 
-	public $filename;
+	private $filename;
+	private $handle;
 
-public function logMessage($logLevel, $message)
+	public function logMessage($logLevel, $message)
 	{
-	// Creates variables for the formatted times 
-	date_default_timezone_set("America/Chicago");
-    $date = date("Y-m-d");
-    $this->filename = "log-{$date}.log";  // reuses the definition of $filename globally
+		$this->setFileName();
 
-    if ($logLevel == 'info') {
-    	return $this->logInfo();
-    } elseif ($logLevel == 'error') {
-    	return $this->logError();
-    } else {
-    	return "[UNK], '{$logLevel}' is unknown.";
-    }
+	    if ($logLevel == 'info') {
+	    	return $this->logInfo();
+	    } elseif ($logLevel == 'error') {
+	    	return $this->logError();
+	    } else {
+	    	return "[UNK], '{$logLevel}' is unknown.";
+	    }
+	}
+
+	protected function setFileName()
+	{
+		date_default_timezone_set("America/Chicago");
+	    $date = date("Y-m-d");
+	    $this->filename = "log-{$date}.log";  // reuses the definition of $filename globally		
 	}
 
 // The logInfo and logError functions create different messages to be printed out by the logMessages functions. 
@@ -35,9 +40,12 @@ public function logError()
 // The fwrite line adds the message, $formatedDateTimeEntry to the new or opened file.
 public function writeFile($message) 
 {
-	$handle = fopen($this->filename, 'a');
-	fwrite($handle, PHP_EOL . $message);
-	fclose($handle);	
+	$this->handle = fopen($this->filename, 'a'); //$this uses a variable that is available across all the methods in the class
+	if (is_writable($this->filename)){
+		fwrite($this->handle, PHP_EOL . $message);  //$this allows us to use the property of a class instead of a local variable
+		fclose($this->handle);	
+	}
+	
 }
 
 }
