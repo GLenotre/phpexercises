138848835dc5946e15ac2f26d90a5b75102e2ce2
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-23 11:27:28 -0500

    created getters and setters for the Rectangle class to work with private properties of height and width

--- rectangle.php
+++ rectangle.php
@@ -1,14 +1,35 @@
 <?php
 
 
-class Rectangle {
-	public $height;
-	public $width;
+class Rectangle 
+{
+	private $height;
+	private $width;
 
 	public function __construct($height, $width)
     {
         $this->height = $height;
         $this->width  = $width;
+    }
+
+    protected function setHeight($height)
+    {
+    	$this->height = $height;
+    }
+
+    protected function getHeight()
+    {
+    	return $this->height;
+    }
+
+    protected function setWidth($width)
+    {
+    	$this->width = $width;
+    }
+
+    protected function getWidth()
+    {
+    	return $this->width;
     }
 
     function area() {
